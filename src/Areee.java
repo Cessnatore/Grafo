6442a73f2228a796e197ce4c6d5b534d6f48121b
Cessna <castillonnfernando@gmail.com>
2016-11-16 03:36:06 -0300

    nada

--- src/Areee.java
+++ src/Areee.java
@@ -7,6 +7,9 @@ public class Areee {
 		putos.agregarNodo("los","oh wa ah ah");
 		putos.agregarNodo("las","los");
         putos.agregarNodo("loma","las");
+        putos.agregarNodo("lema","las");
+        putos.agregarNodo("lemas","lema");
+        putos.agregarNodo("lemur","lemas");
         putos.agregarNodo("disturbed","oh wa ah ah");
 		for(List<String> as : putos.caminos("las","oh wa ah ah")){
             for(String a:as) {
