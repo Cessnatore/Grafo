2917a66fca4be9aa76683fff8b15ed24465c44a6
Cessna <castillonnfernando@gmail.com>
2016-11-16 03:31:14 -0300

    Initial commit

---
+++ src/Areee.java
@@ -0,0 +1,18 @@
+import java.util.List;
+
+public class Areee {
+
+	public static void main(String[] args) {
+		Grafo<String> putos=new Grafo<String>("oh wa ah ah");
+		putos.agregarNodo("los","oh wa ah ah");
+		putos.agregarNodo("las","los");
+        putos.agregarNodo("loma","las");
+        putos.agregarNodo("disturbed","oh wa ah ah");
+		for(List<String> as : putos.caminos("las","oh wa ah ah")){
+            for(String a:as) {
+                System.out.println(a);
+            }
+		}
+	}
+
+}
